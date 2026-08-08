8177e4452310c45a6677a8e3d2cb165ece294708
hamriramzi1-lab <hamriramzi1@gmail.com>
2026-08-08 04:22:39 +0100

    Create debts_screen.dart

---
+++ lib/debts_screen.dart
@@ -0,0 +1,20 @@
+// lib/screens/debts_screen.dart
+// Écran de gestion des dettes
+
+import 'package:flutter/material.dart';
+
+class DebtsScreen extends StatefulWidget {
+  const DebtsScreen({super.key});
+
+  @override
+  State<DebtsScreen> createState() => _DebtsScreenState();
+}
+
+class _DebtsScreenState extends State<DebtsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Écran Dettes (à venir)'),
+    );
+  }
+}
