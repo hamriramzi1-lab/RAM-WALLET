012b2510f738f274239e4a99fa7095e6c5ca248a
hamriramzi1-lab <hamriramzi1@gmail.com>
2026-08-08 04:21:36 +0100

    Create stats_screen.dart

---
+++ lib/stats_screen.dart
@@ -0,0 +1,20 @@
+// lib/screens/stats_screen.dart
+// Écran des statistiques
+
+import 'package:flutter/material.dart';
+
+class StatsScreen extends StatefulWidget {
+  const StatsScreen({super.key});
+
+  @override
+  State<StatsScreen> createState() => _StatsScreenState();
+}
+
+class _StatsScreenState extends State<StatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Écran Statistiques (à venir)'),
+    );
+  }
+}
